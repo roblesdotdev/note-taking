d9f99a4602b1b0d38f357dcd0805725001445819
roblesdotdev <httpsnip@gmail.com>
2025-01-25 11:10:41 -0300

    setup format rules with prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-const MODE = process.env.NODE_ENV;
+const MODE = process.env.NODE_ENV
 
 export default defineConfig({
   css: {
@@ -13,11 +13,11 @@ export default defineConfig({
     },
   },
   build: {
-    cssMinify: MODE === "production",
-    target: "es2023",
+    cssMinify: MODE === 'production',
+    target: 'es2023',
   },
   plugins: [reactRouter(), tsconfigPaths()],
   server: {
     port: Number(process.env.PORT || 3000),
   },
-});
+})
