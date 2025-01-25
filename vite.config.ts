4dc111f0d42697ceea25ceb21aca6e3fa05d0655
roblesdotdev <httpsnip@gmail.com>
2025-01-25 10:49:43 -0300

    config engines & build stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,20 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const MODE = process.env.NODE_ENV;
+
 export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
+  build: {
+    cssMinify: MODE === "production",
+    target: "es2023",
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
   server: {
     port: Number(process.env.PORT || 3000),
   },
-  plugins: [reactRouter(), tsconfigPaths()],
 });
